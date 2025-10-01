6a106bc5284b4f402b64d3e751c4f4d4c2b9a85c
jackhassenzahl <jackson.hassenzahl@gmail.com>
2025-10-01 14:18:14 -0400

    main

--- dev/scripts/navbar.js
+++
@@ -1,6 +0,0 @@
-fetch('/templates/navbar.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('navigation').innerHTML = data;
-  })
-  .catch(error => console.error('Error loading navbar:', error));
