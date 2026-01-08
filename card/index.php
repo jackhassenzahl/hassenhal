ec1967b9cb812ffad202bee612ee42ff25f2fca4
jackhassenzahl <jackson.hassenzahl@gmail.com>
2026-01-08 10:25:47 -0500

    maybe?

--- card/index.php
+++ card/index.php
@@ -27,10 +27,30 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
 <!DOCTYPE html>
 <html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hassenhal</title>
+    <link rel="icon" href="/images/logo.svg" type="image/svg">
+    <link rel="stylesheet" type="text/css" href="/style/style.css">
+    <link rel="stylesheet" type="text/css" media="(max-width: 700px)" href="/style/small.css">
+    <link rel="stylesheet" type="text/css" media="(min-width: 701px) and (max-width: 900px)" href="/style/medium.css">
+    <link rel="stylesheet" type="text/css" media="(min-width: 901px)" href="/style/large.css">
+</head>
 <body>
-<form method="POST">
-    <input type="text" name="qrtext" required>
-    <button type="submit">Download STL</button>
-</form>
+    <div id="navigation"></div>
+    <main>
+        <header>
+            <h1>Under Construction<span id="sub-text"> (Always)</span></h1>
+        </header>
+        <form method="POST">
+            <input type="text" name="qrtext" required>
+            <button type="submit">Download STL</button>
+        </form>
+    </main>
+    <footer>
+        <p>© 2025 Jack Hassenzahl</p>
+    </footer>
+    <script src="/scripts/navbar.js"></script>
 </body>
 </html>
